c384ae5d5edbcd285ab58f6fe33947c59512870a
azhu159 <azhu159@gmail.com>
2023-01-27 15:23:19 +0800

    to github

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import SearchIcon from './search.svg'
+import MovieCard from "./MovieCard";
+const API_URL = 'http://www.omdbapi.com?apikey=9c3bc3c4'
+
+const movie1 = {
+  "Title": "Amazing Spiderman Syndrome", 
+  "Year": "2012", 
+  "imdbID": "tt2586634", 
+  "Type": "movie", 
+  "Poster": "N/A"
+}
 
 function App() {
+  const [movies, setMovies] = useState([])
+  const [searchTerm, setSearchTerm] = useState('Spiderman')
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`)
+    const data = await response.json()
+    console.log(data.Search)
+    setMovies(data.Search)
+  } 
+
+  useEffect(()=>{
+    searchMovies('Spiderman')
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>MovieLand</h1>
+
+      <div className="search">
+        <input 
+          placeholder="Search for movies"
+          value={searchTerm}
+          onChange={(e)=>setSearchTerm(e.target.value)}
+        />
+        <img 
+          src={SearchIcon}
+          alt='search'
+          onClick={()=>searchMovies(searchTerm)}
+        />
+      </div>
+
+      {
+        movies?.length > 0
+        ? <div className="container">
+            {movies.map(movie=><MovieCard key={movie.imdbID} movie1={movie} />)}
+          </div>
+        : <div className="empty">
+            <h2>No movies found</h2>
+        </div>
+      }
+
     </div>
   );
 }
